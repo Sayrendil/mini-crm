e341700e6ec73a90427c91ada03062df9f4675df
Daniyar Sigaev <daniyarsigaev@mail.ru>
2026-04-02 14:47:51 +0500

    feat(enities): create Customer and Ticket enities and migrations

---
+++ app/Models/Ticket.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Customer;
+
+class Ticket extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'customer_id',
+        'subject',
+        'description',
+        'status',
+        'replied_at',
+    ];
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
+}
